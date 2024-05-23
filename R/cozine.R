dd7b2cc658b18058b724b8f7a4f574e6c6117fbf
mariottopier0 <133961826+mariottopier0@users.noreply.github.com>
2024-05-23 11:18:38 +0200

    fixed input parameters

--- R/cozine.R
+++ R/cozine.R
@@ -1,21 +1,17 @@
-cozine <-
-function(dat,fixed = NULL, parallel = TRUE,indices=NULL,lambda.min.ratio=0.5){
-  ### Input ###
+### Input ###
   ######### dat :  OTU table with nx p matrix with n samples and p texa
   ######### fixed: data.frame of fixed covariates (to be conditioned upon)
   ######### parallel: parallelize over variables using "mclapply"?
-  ###########nlambda: number of lambda values on grid (default 200).
-  
-  ##########lambda.min.ratio: minimum lambda ratio (as a function of lambda0, where
-        #                                  the first predictor enters; default .1)
-  
-  # parallelization.
-   ### Output: Object from fitHurdle
+  ######### nlambda: number of lambda values on grid (default 200).
+  ######### lambda.min.ratio: minimum lambda ratio (as a function of lambda0, default .5)
+
+### Output: Object from fitHurdle
+cozine <- function(dat,fixed = NULL, parallel = TRUE,indices=NULL, nlambda=50, lambda.min.ratio=0.5){
   stopifnot(is.matrix(dat))
   p = ncol(dat)
   n = nrow(dat)
   dat_t = clr(dat)
   dat_trans <- conditionalCenter(dat_t)
-  hf = fitHurdle(dat_trans,  fixed=fixed,lambda.min.ratio = lambda.min.ratio, parallel=parallel, control=list(debug=0),keepNodePaths = TRUE) # Using 2 cores
+  hf = fitHurdle(dat_trans, fixed=fixed, parallel=parallel, keepNodePaths = TRUE, control=list(debug=0), nlambda = nlambda, lambda.min.ratio = lambda.min.ratio)
   return(hf) 
 }
